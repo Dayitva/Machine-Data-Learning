8e6775ea0491988b275cdfd0da81696349f6b299
prajneya <prajneya@gmail.com>
2021-03-02 19:13:27 +0530

    add: X-Men First Class

--- Genetic Algorithms/main.py
+++ Genetic Algorithms/main.py
@@ -16,18 +16,18 @@ pop_size2 = 10
 def fitness(error1, error2):
     return 1/(error1 + error2)
 
-def mutation(population):
-    mutation_factor = random.uniform(-1, 1)
-    return [random.choice([i, i*mutation_factor]) for i in population]
+def mutation(population, probability, mutate_from, mutate_till):
+    mutation_factor = random.uniform(mutate_from, mutate_till)
+    return [random.choice([i, i*mutation_factor], p=[probability, 1-probability]) for i in population]
 
-def crossover(parent1, parent2):
+def crossover(parent1, parent2, probability, mutate_from, mutate_till):
     length1 = int(np.size(parent1)/2)
     length2 = int(np.size(parent2)/2)
 
     crossedParent1 = np.concatenate((parent1[:length1], parent2[length2:]))
     crossedParent2 = np.concatenate((parent1[length1:], parent2[:length2]))
 
-    return crossedParent1, crossedParent2
+    return mutation(crossedParent1, probability, mutate_from, mutate_till), mutation(crossedParent2, probability, mutate_from, mutate_till)
 
 generation = 0
 fitness_pop = np.zeros(pop_size2)
@@ -35,7 +35,7 @@ prob_pop = np.zeros(pop_size2)
 parents = [0 for i in range(10)]
 
 for i in range(10):
-    parents[i] = np.copy(mutation(initial_population))
+    parents[i] = np.copy(mutation(initial_population, 0.9, 0.9, 1.1))
 
 fertile_parents = np.zeros(pop_size2)
 
@@ -43,6 +43,8 @@ children = np.zeros(pop_size2)
 
 colonization = []
 colonization_fitness = []
+
+crossover_probability = 0.8
 
 while generation < pop_size2:
     for i in range(pop_size2):
@@ -73,7 +75,7 @@ while generation < pop_size2:
     mate_count = 0
 
     while mate_count < 10:
-        offspring1, offspring2 = crossover(parents[fertile_parents_idx[mate_count]], parents[fertile_parents_idx[mate_count+1]])
+        offspring1, offspring2 = crossover(parents[fertile_parents_idx[mate_count]], parents[fertile_parents_idx[mate_count+1]], crossover_probability, 0.9, 1.1)
         parents[mate_count] = offspring1
         parents[mate_count+1] = offspring2
         mate_count += 2
@@ -86,7 +88,9 @@ while generation < pop_size2:
     with open("logs.txt", "a", encoding="utf8") as text_file:
         print("====================================================", file=text_file)
 
-    population = len(colonization_fitness)
+    crossover_probability -= 1
+
+population = len(colonization_fitness)
 
 max_idx = 0
 alpha_fitness = colonization_fitness[max_idx]
