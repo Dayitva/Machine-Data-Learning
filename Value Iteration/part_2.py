3a173af247b111102a0b44bb202779cc25d791c8
Dayitva <dayitvagoel@gmail.com>
2021-04-08 22:51:30 +0700

    add: Final Code with trace

--- Value Iteration/part_2.py
+++ Value Iteration/part_2.py
@@ -1,7 +1,5 @@
 import numpy as np
-from copy import deepcopy
-from functools import reduce
-from operator import add
+import copy
 import os
 
 NEGATIVE_INF = np.NINF
@@ -12,17 +10,11 @@ MATERIAL_RANGE = 3
 POSITION_RANGE = 5
 MOOD_RANGE = 2
 
-HEALTH_VALUES = tuple(range(HEALTH_RANGE))
-ARROWS_VALUES = tuple(range(ARROWS_RANGE))
-MATERIAL_VALUES = tuple(range(MATERIAL_RANGE))
-POSITION_VALUES = tuple(range(POSITION_RANGE))
-MOOD_VALUES = tuple(range(MOOD_RANGE))
-
-HEALTH_FACTOR = 25 # 0, 25, 50, 75, 100
-ARROWS_FACTOR = 1 # 0, 1, 2, 3
-MATERIAL_FACTOR = 1 # 0, 1, 2
-POSITION_FACTOR = 1 # (0, north), (1, east), (2, south), (3, west), (4, center)
-MOOD_FACTOR = 1 # (0, dormant), (1, ready)
+HEALTH_VALUES = tuple(range(HEALTH_RANGE)) # 0, 25, 50, 75, 100
+ARROWS_VALUES = tuple(range(ARROWS_RANGE)) # 0, 1, 2, 3
+MATERIAL_VALUES = tuple(range(MATERIAL_RANGE)) # 0, 1, 2
+POSITION_VALUES = tuple(range(POSITION_RANGE)) # (0, north), (1, east), (2, south), (3, west), (4, center)
+MOOD_VALUES = tuple(range(MOOD_RANGE)) # (0, dormant), (1, ready)
 
 POSITION_ARRAY = ["N", "E", "S", "W", "C"]
 HEALTH_ARRAY = [0, 25, 50, 75, 100]
@@ -40,26 +32,22 @@ ACTION_HIT = 6
 ACTION_CRAFT = 7
 ACTION_GATHER = 8
 
-TEAM = 21
-Y = 0.5
-PRIZE = 50
-COST = -20
-
 GAMMA = 0.999
 DELTA = 0.001
+PRIZE = 50
+PUNISHMENT = -40
+COST = -20
 
 REWARD = np.zeros((POSITION_RANGE, HEALTH_RANGE, ARROWS_RANGE, MATERIAL_RANGE, MOOD_RANGE))
 REWARD[:, 0, :, :, :] = PRIZE
+
 # print(REWARD)
 
 class State:
-    def __init__(self, position, enemy_health, num_arrows, materials, mood):
-        if (enemy_health not in HEALTH_VALUES) or (num_arrows not in ARROWS_VALUES) or (materials not in MATERIAL_VALUES):
-            raise ValueError
-
+    def __init__(self, position, enemy_health, arrows, materials, mood):
         self.position = position
         self.health = enemy_health
-        self.arrows = num_arrows
+        self.arrows = arrows
         self.materials = materials
         self.mood = mood
 
@@ -67,8 +55,6 @@ class State:
         return (self.position, self.health, self.arrows, self.materials, self.mood)
 
 def action(action_type, state, costs):
-    # returns cost, array of tuple of (probability, state)
-
     state = State(*state)
 
     if action_type == ACTION_SHOOT:
@@ -88,7 +74,7 @@ def action(action_type, state, costs):
                 choices.append((0.08, State(state.position, state.health, new_arrows, state.materials, 0)))
             elif state.mood == 1:
                 choices.append((0.5, State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0)))
-                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = -40
+                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = PUNISHMENT
                 choices.append((0.45, State(state.position, max(HEALTH_VALUES[0], state.health-1), new_arrows, state.materials, 1)))
                 choices.append((0.05, State(state.position, state.health, new_arrows, state.materials, 1)))
 
@@ -116,7 +102,7 @@ def action(action_type, state, costs):
                 choices.append((0.4, State(state.position, state.health, new_arrows, state.materials, 0)))
             elif state.mood == 1:
                 choices.append((0.5, State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0)))
-                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = -40
+                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = PUNISHMENT
                 choices.append((0.25, State(state.position, max(HEALTH_VALUES[0], state.health-1), 0, state.materials, 1)))
                 choices.append((0.25, State(state.position, state.health, 0, state.materials, 1)))
         else:
@@ -141,7 +127,7 @@ def action(action_type, state, costs):
                 choices.append((0.64, State(state.position, state.health, state.arrows, state.materials, 0)))
             elif state.mood == 1:
                 choices.append((0.5, State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0)))
-                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = -40
+                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = PUNISHMENT
                 choices.append((0.1, State(state.position, max(HEALTH_VALUES[0], state.health-2), state.arrows, state.materials, 1)))
                 choices.append((0.4, State(state.position, state.health, state.arrows, state.materials, 1)))
 
@@ -155,7 +141,7 @@ def action(action_type, state, costs):
                 choices.append((0.72, State(state.position, state.health, state.arrows, state.materials, 0)))
             elif state.mood == 1:
                 choices.append((0.5, State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0)))
-                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = -40
+                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = PUNISHMENT
                 choices.append(
                     (0.05, State(state.position, max(HEALTH_VALUES[0], state.health-2), state.arrows, state.materials, 1)))
                 choices.append((0.45, State(state.position, state.health, state.arrows, state.materials, 1)))
@@ -259,7 +245,7 @@ def action(action_type, state, costs):
                 choices.append((0.12, State(1, state.health, state.arrows, state.materials, 0)))
             elif state.mood == 1:
                 choices.append((0.5, State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0)))
-                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = -40
+                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = PUNISHMENT
                 choices.append((0.425, State(0, state.health, state.arrows, state.materials, 1)))
                 choices.append((0.075, State(1, state.health, state.arrows, state.materials, 1)))
         else:
@@ -293,7 +279,7 @@ def action(action_type, state, costs):
                 choices.append((0.12, State(1, state.health, state.arrows, state.materials, 0)))
             elif state.mood == 1:
                 choices.append((0.5, State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0)))
-                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = -40
+                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = PUNISHMENT
                 choices.append((0.425, State(3, state.health, state.arrows, state.materials, 1)))
                 choices.append((0.075, State(1, state.health, state.arrows, state.materials, 1)))
         else:
@@ -323,7 +309,7 @@ def action(action_type, state, costs):
                 choices.append((0.12, State(1, state.health, state.arrows, state.materials, 0)))
             elif state.mood == 1:
                 choices.append((0.5, State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0)))
-                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = -40
+                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = PUNISHMENT
                 choices.append((0.425, State(1, state.health, state.arrows, state.materials, 1)))
                 choices.append((0.075, State(1, state.health, state.arrows, state.materials, 1)))
         else:
@@ -344,7 +330,7 @@ def action(action_type, state, costs):
                 choices.append((0.8, State(4, state.health, state.arrows, state.materials, 0)))
             elif state.mood == 1:
                 choices.append((0.5, State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0)))
-                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = -40
+                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = PUNISHMENT
                 choices.append((0.5, State(4, state.health, state.arrows, state.materials, 1)))
         elif state.position == 4:
             if state.mood == 0:
@@ -354,7 +340,7 @@ def action(action_type, state, costs):
                 choices.append((0.12, State(3, state.health, state.arrows, state.materials, 0)))
             elif state.mood == 1:
                 choices.append((0.5, State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0)))
-                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = -40
+                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = PUNISHMENT
                 choices.append((0.425, State(3, state.health, state.arrows, state.materials, 1)))
                 choices.append((0.075, State(3, state.health, state.arrows, state.materials, 1)))
         else:
@@ -386,7 +372,7 @@ def action(action_type, state, costs):
                 choices.append((0.8, State(1, state.health, state.arrows, state.materials, 0)))
             elif state.mood == 1:
                 choices.append((0.5, State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0)))
-                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = -40
+                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = PUNISHMENT
                 choices.append((0.5, State(4, state.health, state.arrows, state.materials, 1)))
         elif state.position == 2:
             if state.mood == 0:
@@ -414,7 +400,7 @@ def action(action_type, state, costs):
                 choices.append((0.12, State(1, state.health, state.arrows, state.materials, 0)))
             elif state.mood == 1:
                 choices.append((0.5, State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0)))
-                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = -40
+                REWARD[State(state.position, min(HEALTH_VALUES[-1], state.health+1), 0, state.materials, 0).show()] = PUNISHMENT
                 choices.append((0.425, State(4, state.health, state.arrows, state.materials, 1)))
                 choices.append((0.075, State(1, state.health, state.arrows, state.materials, 1)))
         else:
@@ -426,128 +412,145 @@ def action(action_type, state, costs):
 
         return cost, choices
 
-def show(i, utilities, policies, path):
-    with open(path, 'a+') as f:
-        f.write('iteration={}\n'.format(i+1))
+def dump(iteration, utilities, policies, file):
+    with open(file, 'a', encoding="utf8") as text_file:
+        print("iteration={}".format(iteration), file=text_file)
         utilities = np.around(utilities, 3)
+        
         for state, util in np.ndenumerate(utilities):
             state = State(*state)
 
             if state.health == 0:
-                f.write('("{}",{},{},"{}",{}):"NONE",\n'.format(POSITION_ARRAY[state.position], state.materials, state.arrows, MOOD_ARRAY[state.mood], HEALTH_ARRAY[state.health]))
+                # print('("{}",{},{},"{}",{}):"NONE",'.format(POSITION_ARRAY[state.position], state.materials, state.arrows, MOOD_ARRAY[state.mood], HEALTH_ARRAY[state.health]), file=text_file)
+                print("({},{},{},{},{}):NONE=[{}]".format(POSITION_ARRAY[state.position], state.materials, state.arrows, MOOD_ARRAY[state.mood], HEALTH_ARRAY[state.health], util), file=text_file)
                 continue
 
-            act_str = ""
+            action = ""
 
-            if policies[state.show()] == ACTION_SHOOT:
-                act_str = 'SHOOT'
-            elif policies[state.show()] == ACTION_HIT:
-                act_str = 'HIT'
-            elif policies[state.show()] == ACTION_CRAFT:
-                act_str = 'CRAFT'
-            elif policies[state.show()] == ACTION_GATHER:
-                act_str = 'GATHER'
-            elif policies[state.show()] == ACTION_MOVE_UP:
-                act_str = 'UP'
+            if policies[state.show()] == ACTION_MOVE_UP:
+                action = 'UP'
             elif policies[state.show()] == ACTION_MOVE_DOWN:
-                act_str = 'DOWN'
+                action = 'DOWN'
             elif policies[state.show()] == ACTION_MOVE_LEFT:
-                act_str = 'LEFT'
+                action = 'LEFT'
             elif policies[state.show()] == ACTION_MOVE_RIGHT:
-                act_str = 'RIGHT'
+                action = 'RIGHT'
             elif policies[state.show()] == ACTION_STAY:
-                act_str = 'STAY'
+                action = 'STAY'
+            elif policies[state.show()] == ACTION_SHOOT:
+                action = 'SHOOT'
+            elif policies[state.show()] == ACTION_HIT:
+                action = 'HIT'
+            elif policies[state.show()] == ACTION_CRAFT:
+                action = 'CRAFT'
+            elif policies[state.show()] == ACTION_GATHER:
+                action = 'GATHER'
 
-            f.write('("{}",{},{},"{}",{}):"{}",\n'.format(POSITION_ARRAY[state.position], state.materials, state.arrows, MOOD_ARRAY[state.mood], HEALTH_ARRAY[state.health], act_str))
-        f.write('\n')
+            # print('("{}",{},{},"{}",{}):"{}",'.format(POSITION_ARRAY[state.position], state.materials, state.arrows, MOOD_ARRAY[state.mood], HEALTH_ARRAY[state.health], action), file=text_file)
+            print("({},{},{},{},{}):{}=[{}]".format(POSITION_ARRAY[state.position], state.materials, state.arrows, MOOD_ARRAY[state.mood], HEALTH_ARRAY[state.health], action, util), file=text_file)
+        print("", file=text_file)
 
 def util_addition(utilities, states):
 
     final_util = 0
 
     for state in states:
-        add_factor = state[0]*utilities[state[1].show()]
+        add_factor = state[0] * utilities[state[1].show()]
         final_util += add_factor
 
     return final_util
 
+def calculate_utilities(temp, utilities, delta, gamma_inp, costs_inp):
+    for state, util in np.ndenumerate(utilities):
 
-def value_iteration(delta_inp, gamma_inp, costs_inp, path):
+        if state[1] == 0: # MM's Health is 0
+            continue
+
+        new_util = NEGATIVE_INF
+        for action_number in range(NUM_ACTIONS):
+            cost, states = action(action_number, state, costs_inp)
+
+            if cost is None:
+                continue
+
+            expected_util = util_addition(utilities, states)
+            
+            if new_util < cost + (gamma_inp * expected_util):
+                new_util = cost + (gamma_inp * expected_util)
+            # print(expected_util, new_util)
+
+        temp[state] = new_util
+        
+        if delta < abs(util - new_util):
+            delta = abs(util - new_util)
+            
+    return temp, delta
+                
+def calculate_policies(policies, utilities, gamma_inp, costs_inp):
+    for state, temp_util in np.ndenumerate(utilities):
+
+        if state[1] == 0: # MM's health is 0
+            continue
+
+        best_util = NEGATIVE_INF
+        best_action = None
+
+        for action_number in range(NUM_ACTIONS):
+            cost, states = action(action_number, state, costs_inp)
+
+            if states is None:
+                continue
+            
+            temp_util = util_addition(utilities, states)
+            action_util = cost + (gamma_inp * temp_util)
+
+            if action_util > best_util:
+                best_action = action_number
+                best_util = action_util
+
+        policies[state] = best_action
+        
+    return policies
+
+def perform_value_iteration(delta_inp, gamma_inp, costs_inp, path):
     utilities = np.zeros((POSITION_RANGE, HEALTH_RANGE, ARROWS_RANGE, MATERIAL_RANGE, MOOD_RANGE))
-    policies = -1*np.ones((POSITION_RANGE, HEALTH_RANGE, ARROWS_RANGE, MATERIAL_RANGE, MOOD_RANGE))
+    policies = -1 * np.ones((POSITION_RANGE, HEALTH_RANGE, ARROWS_RANGE, MATERIAL_RANGE, MOOD_RANGE))
 
     iteration = 0
     convergence = 1
-
+    
     while convergence == 1:
-        temp = np.zeros(utilities.shape)
         delta = NEGATIVE_INF
+        temp = np.zeros(utilities.shape)
+        
+        temp, delta = calculate_utilities(temp, utilities, delta, gamma_inp, costs_inp)
+        utilities = copy.deepcopy(temp)
+        policies = calculate_policies(policies, utilities, gamma_inp, costs_inp)
 
-        for state, util in np.ndenumerate(utilities):
-
-            if state[1] == 0:
-                continue
-
-            new_util = NEGATIVE_INF
-            for act_index in range(NUM_ACTIONS):
-                cost, states = action(act_index, state, costs_inp)
-
-                if cost is None:
-                    continue
-
-                expected_util = util_addition(utilities, states)
-                new_util = max(new_util, cost + gamma_inp * expected_util)
-                # print(expected_util, new_util)
-
-            temp[state] = new_util
-            delta = max(delta, abs(util - new_util))
-
-        utilities = deepcopy(temp)
-
-        for state, temp_util in np.ndenumerate(utilities):
-
-            if state[1] == 0:
-                continue
-
-            best_util = np.NINF
-            best_action = None
-
-            for act_index in range(NUM_ACTIONS):
-                cost, states = action(act_index, state, costs_inp)
-
-                if states is None:
-                    continue
-
-                action_util = cost + gamma_inp * \
-                    util_addition(utilities, states)
-
-                if action_util > best_util:
-                    best_action = act_index
-                    best_util = action_util
-
-            policies[state] = best_action
-
-        show(iteration, utilities, policies, path)
+        dump(iteration, utilities, policies, path)
+        
         iteration += 1
+        
         if delta < delta_inp:
             convergence = 0
+            
     return iteration
 
-# PREP
+# Make directory
 os.makedirs('outputs', exist_ok=True)
 
 # TASK 1
-path = 'outputs/test_trace.txt'
-value_iteration(DELTA, GAMMA, (COST, COST, COST, COST, COST, COST, COST, COST, COST), path)
+path = 'outputs/part_2_trace.txt'
+perform_value_iteration(DELTA, GAMMA, (COST, COST, COST, COST, COST, COST, COST, COST, COST), path)
 
 # TASK 2 Case 1
-# path = 'outputs/task_2_1_trace.txt'
-# value_iteration(DELTA, GAMMA, (COST, COST, COST, COST, COST, COST, COST, COST, COST), path)
+path = 'outputs/part_2_task_2.1_trace.txt'
+perform_value_iteration(DELTA, GAMMA, (COST, COST, COST, COST, COST, COST, COST, COST, COST), path)
 
 # TASK 2 Case 2
-# path = 'outputs/task_2_2_trace.txt'
-# value_iteration(DELTA, GAMMA, (COST, COST, COST, COST, 0, COST, COST, COST, COST), path)
+path = 'outputs/part_2_task_2.2_trace.txt'
+perform_value_iteration(DELTA, GAMMA, (COST, COST, COST, COST, 0, COST, COST, COST, COST), path)
 #
 # # TASK 2 Case 3
-# path = 'outputs/task_2_3_trace.txt'
-# value_iteration(DELTA, 0.25, (COST, COST, COST, COST, COST, COST, COST, COST, COST), path)
+path = 'outputs/part_2_task_2.3_trace.txt'
+perform_value_iteration(DELTA, 0.25, (COST, COST, COST, COST, COST, COST, COST, COST, COST), path)
