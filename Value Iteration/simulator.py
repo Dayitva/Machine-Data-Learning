3016ab3873a0325d4a6d5930d33a09640533074d
Dayitva <dayitvagoel@gmail.com>
2021-04-08 15:37:50 +0700

    fix: Simuator issues

--- Value Iteration/simulator.py
+++ Value Iteration/simulator.py
@@ -26,29 +26,29 @@ policy = {
 ("N",2,3,"D",0):"NONE",
 ("N",2,3,"R",0):"NONE",
 ("N",0,0,"D",25):"DOWN",
-("N",0,0,"R",25):"DOWN",
+("N",0,0,"R",25):"STAY",
 ("N",1,0,"D",25):"CRAFT",
 ("N",1,0,"R",25):"CRAFT",
 ("N",2,0,"D",25):"CRAFT",
 ("N",2,0,"R",25):"CRAFT",
 ("N",0,1,"D",25):"DOWN",
-("N",0,1,"R",25):"DOWN",
+("N",0,1,"R",25):"STAY",
 ("N",1,1,"D",25):"DOWN",
 ("N",1,1,"R",25):"CRAFT",
 ("N",2,1,"D",25):"DOWN",
 ("N",2,1,"R",25):"CRAFT",
 ("N",0,2,"D",25):"DOWN",
-("N",0,2,"R",25):"DOWN",
+("N",0,2,"R",25):"STAY",
 ("N",1,2,"D",25):"DOWN",
-("N",1,2,"R",25):"CRAFT",
+("N",1,2,"R",25):"STAY",
 ("N",2,2,"D",25):"DOWN",
-("N",2,2,"R",25):"CRAFT",
+("N",2,2,"R",25):"STAY",
 ("N",0,3,"D",25):"DOWN",
-("N",0,3,"R",25):"DOWN",
+("N",0,3,"R",25):"STAY",
 ("N",1,3,"D",25):"DOWN",
-("N",1,3,"R",25):"DOWN",
+("N",1,3,"R",25):"STAY",
 ("N",2,3,"D",25):"DOWN",
-("N",2,3,"R",25):"DOWN",
+("N",2,3,"R",25):"STAY",
 ("N",0,0,"D",50):"DOWN",
 ("N",0,0,"R",50):"DOWN",
 ("N",1,0,"D",50):"CRAFT",
@@ -56,23 +56,23 @@ policy = {
 ("N",2,0,"D",50):"CRAFT",
 ("N",2,0,"R",50):"CRAFT",
 ("N",0,1,"D",50):"DOWN",
-("N",0,1,"R",50):"DOWN",
+("N",0,1,"R",50):"STAY",
 ("N",1,1,"D",50):"CRAFT",
 ("N",1,1,"R",50):"CRAFT",
 ("N",2,1,"D",50):"CRAFT",
 ("N",2,1,"R",50):"CRAFT",
 ("N",0,2,"D",50):"DOWN",
-("N",0,2,"R",50):"DOWN",
+("N",0,2,"R",50):"STAY",
 ("N",1,2,"D",50):"DOWN",
 ("N",1,2,"R",50):"CRAFT",
 ("N",2,2,"D",50):"DOWN",
 ("N",2,2,"R",50):"CRAFT",
 ("N",0,3,"D",50):"DOWN",
-("N",0,3,"R",50):"DOWN",
+("N",0,3,"R",50):"STAY",
 ("N",1,3,"D",50):"DOWN",
-("N",1,3,"R",50):"DOWN",
+("N",1,3,"R",50):"STAY",
 ("N",2,3,"D",50):"DOWN",
-("N",2,3,"R",50):"DOWN",
+("N",2,3,"R",50):"STAY",
 ("N",0,0,"D",75):"DOWN",
 ("N",0,0,"R",75):"DOWN",
 ("N",1,0,"D",75):"CRAFT",
@@ -80,23 +80,23 @@ policy = {
 ("N",2,0,"D",75):"CRAFT",
 ("N",2,0,"R",75):"CRAFT",
 ("N",0,1,"D",75):"DOWN",
-("N",0,1,"R",75):"DOWN",
+("N",0,1,"R",75):"STAY",
 ("N",1,1,"D",75):"CRAFT",
 ("N",1,1,"R",75):"CRAFT",
 ("N",2,1,"D",75):"CRAFT",
 ("N",2,1,"R",75):"CRAFT",
 ("N",0,2,"D",75):"DOWN",
-("N",0,2,"R",75):"DOWN",
+("N",0,2,"R",75):"STAY",
 ("N",1,2,"D",75):"CRAFT",
 ("N",1,2,"R",75):"CRAFT",
 ("N",2,2,"D",75):"CRAFT",
 ("N",2,2,"R",75):"CRAFT",
 ("N",0,3,"D",75):"DOWN",
-("N",0,3,"R",75):"DOWN",
+("N",0,3,"R",75):"STAY",
 ("N",1,3,"D",75):"DOWN",
-("N",1,3,"R",75):"DOWN",
+("N",1,3,"R",75):"STAY",
 ("N",2,3,"D",75):"DOWN",
-("N",2,3,"R",75):"DOWN",
+("N",2,3,"R",75):"STAY",
 ("N",0,0,"D",100):"DOWN",
 ("N",0,0,"R",100):"DOWN",
 ("N",1,0,"D",100):"CRAFT",
@@ -272,7 +272,7 @@ policy = {
 ("S",2,0,"D",25):"UP",
 ("S",2,0,"R",25):"UP",
 ("S",0,1,"D",25):"UP",
-("S",0,1,"R",25):"STAY",
+("S",0,1,"R",25):"GATHER",
 ("S",1,1,"D",25):"UP",
 ("S",1,1,"R",25):"GATHER",
 ("S",2,1,"D",25):"UP",
@@ -286,7 +286,7 @@ policy = {
 ("S",0,3,"D",25):"UP",
 ("S",0,3,"R",25):"STAY",
 ("S",1,3,"D",25):"UP",
-("S",1,3,"R",25):"STAY",
+("S",1,3,"R",25):"GATHER",
 ("S",2,3,"D",25):"UP",
 ("S",2,3,"R",25):"STAY",
 ("S",0,0,"D",50):"GATHER",
@@ -386,20 +386,20 @@ policy = {
 ("W",2,3,"D",0):"NONE",
 ("W",2,3,"R",0):"NONE",
 ("W",0,0,"D",25):"RIGHT",
-("W",0,0,"R",25):"RIGHT",
+("W",0,0,"R",25):"STAY",
 ("W",1,0,"D",25):"RIGHT",
-("W",1,0,"R",25):"RIGHT",
+("W",1,0,"R",25):"STAY",
 ("W",2,0,"D",25):"RIGHT",
 ("W",2,0,"R",25):"RIGHT",
 ("W",0,1,"D",25):"RIGHT",
-("W",0,1,"R",25):"RIGHT",
+("W",0,1,"R",25):"STAY",
 ("W",1,1,"D",25):"RIGHT",
-("W",1,1,"R",25):"SHOOT",
+("W",1,1,"R",25):"STAY",
 ("W",2,1,"D",25):"RIGHT",
-("W",2,1,"R",25):"RIGHT",
-("W",0,2,"D",25):"RIGHT",
+("W",2,1,"R",25):"STAY",
+("W",0,2,"D",25):"SHOOT",
 ("W",0,2,"R",25):"SHOOT",
-("W",1,2,"D",25):"RIGHT",
+("W",1,2,"D",25):"SHOOT",
 ("W",1,2,"R",25):"SHOOT",
 ("W",2,2,"D",25):"SHOOT",
 ("W",2,2,"R",25):"SHOOT",
@@ -410,23 +410,23 @@ policy = {
 ("W",2,3,"D",25):"SHOOT",
 ("W",2,3,"R",25):"SHOOT",
 ("W",0,0,"D",50):"RIGHT",
-("W",0,0,"R",50):"RIGHT",
+("W",0,0,"R",50):"STAY",
 ("W",1,0,"D",50):"RIGHT",
 ("W",1,0,"R",50):"RIGHT",
 ("W",2,0,"D",50):"RIGHT",
 ("W",2,0,"R",50):"RIGHT",
 ("W",0,1,"D",50):"RIGHT",
-("W",0,1,"R",50):"RIGHT",
+("W",0,1,"R",50):"STAY",
 ("W",1,1,"D",50):"RIGHT",
-("W",1,1,"R",50):"RIGHT",
+("W",1,1,"R",50):"STAY",
 ("W",2,1,"D",50):"RIGHT",
-("W",2,1,"R",50):"RIGHT",
+("W",2,1,"R",50):"STAY",
 ("W",0,2,"D",50):"RIGHT",
-("W",0,2,"R",50):"RIGHT",
+("W",0,2,"R",50):"STAY",
 ("W",1,2,"D",50):"RIGHT",
-("W",1,2,"R",50):"RIGHT",
+("W",1,2,"R",50):"STAY",
 ("W",2,2,"D",50):"RIGHT",
-("W",2,2,"R",50):"RIGHT",
+("W",2,2,"R",50):"STAY",
 ("W",0,3,"D",50):"RIGHT",
 ("W",0,3,"R",50):"SHOOT",
 ("W",1,3,"D",50):"RIGHT",
@@ -440,11 +440,11 @@ policy = {
 ("W",2,0,"D",75):"RIGHT",
 ("W",2,0,"R",75):"RIGHT",
 ("W",0,1,"D",75):"RIGHT",
-("W",0,1,"R",75):"RIGHT",
+("W",0,1,"R",75):"STAY",
 ("W",1,1,"D",75):"RIGHT",
-("W",1,1,"R",75):"RIGHT",
+("W",1,1,"R",75):"STAY",
 ("W",2,1,"D",75):"RIGHT",
-("W",2,1,"R",75):"RIGHT",
+("W",2,1,"R",75):"STAY",
 ("W",0,2,"D",75):"RIGHT",
 ("W",0,2,"R",75):"SHOOT",
 ("W",1,2,"D",75):"RIGHT",
@@ -452,11 +452,11 @@ policy = {
 ("W",2,2,"D",75):"RIGHT",
 ("W",2,2,"R",75):"SHOOT",
 ("W",0,3,"D",75):"RIGHT",
-("W",0,3,"R",75):"SHOOT",
+("W",0,3,"R",75):"STAY",
 ("W",1,3,"D",75):"RIGHT",
-("W",1,3,"R",75):"SHOOT",
+("W",1,3,"R",75):"STAY",
 ("W",2,3,"D",75):"RIGHT",
-("W",2,3,"R",75):"SHOOT",
+("W",2,3,"R",75):"STAY",
 ("W",0,0,"D",100):"RIGHT",
 ("W",0,0,"R",100):"RIGHT",
 ("W",1,0,"D",100):"RIGHT",
@@ -464,17 +464,17 @@ policy = {
 ("W",2,0,"D",100):"RIGHT",
 ("W",2,0,"R",100):"RIGHT",
 ("W",0,1,"D",100):"RIGHT",
-("W",0,1,"R",100):"RIGHT",
+("W",0,1,"R",100):"STAY",
 ("W",1,1,"D",100):"RIGHT",
 ("W",1,1,"R",100):"RIGHT",
 ("W",2,1,"D",100):"RIGHT",
 ("W",2,1,"R",100):"RIGHT",
 ("W",0,2,"D",100):"RIGHT",
-("W",0,2,"R",100):"RIGHT",
+("W",0,2,"R",100):"STAY",
 ("W",1,2,"D",100):"RIGHT",
-("W",1,2,"R",100):"RIGHT",
+("W",1,2,"R",100):"STAY",
 ("W",2,2,"D",100):"RIGHT",
-("W",2,2,"R",100):"RIGHT",
+("W",2,2,"R",100):"STAY",
 ("W",0,3,"D",100):"RIGHT",
 ("W",0,3,"R",100):"SHOOT",
 ("W",1,3,"D",100):"RIGHT",
@@ -506,13 +506,13 @@ policy = {
 ("C",2,3,"D",0):"NONE",
 ("C",2,3,"R",0):"NONE",
 ("C",0,0,"D",25):"RIGHT",
-("C",0,0,"R",25):"RIGHT",
+("C",0,0,"R",25):"LEFT",
 ("C",1,0,"D",25):"UP",
 ("C",1,0,"R",25):"UP",
 ("C",2,0,"D",25):"UP",
 ("C",2,0,"R",25):"UP",
 ("C",0,1,"D",25):"RIGHT",
-("C",0,1,"R",25):"SHOOT",
+("C",0,1,"R",25):"UP",
 ("C",1,1,"D",25):"RIGHT",
 ("C",1,1,"R",25):"SHOOT",
 ("C",2,1,"D",25):"RIGHT",
@@ -525,7 +525,7 @@ policy = {
 ("C",2,2,"R",25):"LEFT",
 ("C",0,3,"D",25):"RIGHT",
 ("C",0,3,"R",25):"LEFT",
-("C",1,3,"D",25):"RIGHT",
+("C",1,3,"D",25):"SHOOT",
 ("C",1,3,"R",25):"LEFT",
 ("C",2,3,"D",25):"SHOOT",
 ("C",2,3,"R",25):"LEFT",
@@ -544,9 +544,9 @@ policy = {
 ("C",0,2,"D",50):"RIGHT",
 ("C",0,2,"R",50):"LEFT",
 ("C",1,2,"D",50):"RIGHT",
-("C",1,2,"R",50):"UP",
+("C",1,2,"R",50):"LEFT",
 ("C",2,2,"D",50):"RIGHT",
-("C",2,2,"R",50):"UP",
+("C",2,2,"R",50):"LEFT",
 ("C",0,3,"D",50):"RIGHT",
 ("C",0,3,"R",50):"LEFT",
 ("C",1,3,"D",50):"RIGHT",
@@ -600,20 +600,21 @@ policy = {
 ("C",1,3,"D",100):"RIGHT",
 ("C",1,3,"R",100):"LEFT",
 ("C",2,3,"D",100):"RIGHT",
-("C",2,3,"R",100):"LEFT"
+("C",2,3,"R",100):"LEFT",
 }
 
 def simulate(start, policy, flag):
     cur = list(start) 
 
     while cur[4] > 0:
-        print(tuple(cur), policy[tuple(cur)])
+        curpolicy = policy[tuple(cur)]
+        print(tuple(cur), curpolicy)
 
         if cur[3] == 'D':
             sim_random = rand.uniform(0,1)
             if sim_random <= 0.2:
                 cur[3] = 'R'
-
+        
         elif cur[3] == 'R':
             sim_random = rand.uniform(0,1)
             if sim_random <= 0.5:
@@ -625,10 +626,10 @@ def simulate(start, policy, flag):
                     continue
         
         if cur[0] == 'W':
-            if policy[tuple(cur)] == "RIGHT":
+            if curpolicy == "RIGHT":
                 cur[0] = 'C'
 
-            elif policy[tuple(cur)] == "SHOOT":
+            elif curpolicy == "SHOOT":
                 if cur[2] > 0:
                     cur[2] = cur[2] - 1
 
@@ -637,7 +638,7 @@ def simulate(start, policy, flag):
                         cur[4] = cur[4] - 25
 
         elif cur[0] == 'N':
-            if policy[tuple(cur)] == "DOWN":
+            if curpolicy == "DOWN":
                 sim_random = rand.uniform(0,1)
                 if sim_random <= 0.85:
                     cur[0] = 'C'
@@ -645,7 +646,7 @@ def simulate(start, policy, flag):
                 else:
                     cur[0] = 'E'
 
-            elif policy[tuple(cur)] == "STAY":
+            elif curpolicy == "STAY":
                 sim_random = rand.uniform(0,1)
                 if sim_random <= 0.85:
                     cur[0] = 'N'
@@ -653,7 +654,7 @@ def simulate(start, policy, flag):
                 else:
                     cur[0] = 'E'
 
-            elif policy[tuple(cur)] == "CRAFT":
+            elif curpolicy == "CRAFT":
                 if cur[1] > 0:
                     cur[1] = cur[1] - 1
                     sim_random = rand.uniform(0,1)
@@ -667,26 +668,26 @@ def simulate(start, policy, flag):
                         cur[2] = cur[2] + (cur[2] !=3)
 
         elif cur[0] == 'E':
-            if policy[tuple(cur)] == "LEFT" and flag != 1:
+            if curpolicy == "LEFT" and flag != 1:
                 cur[0] = 'C'
 
-            elif policy[tuple(cur)] == "LEFT" and flag == 1:
+            elif curpolicy == "LEFT" and flag == 1:
                 cur[0] = 'W'
 
-            elif policy[tuple(cur)] == "SHOOT":
+            elif curpolicy == "SHOOT":
                 if cur[2] > 0:
                     cur[2] = cur[2] - 1
                     sim_random = rand.uniform(0,1)
                     if sim_random <= 0.9:
                         cur[4] = cur[4] - 25
 
-            elif policy[tuple(cur)] == "HIT":
+            elif curpolicy == "HIT":
                 sim_random = rand.uniform(0,1)
                 if sim_random <= 0.2:
                     cur[4] = cur[4] - 50
 
         elif cur[0] == 'S':
-            if policy[tuple(cur)] == "UP":
+            if curpolicy == "UP":
                 sim_random = rand.uniform(0,1)
                 if sim_random <= 0.85:
                     cur[0] = 'C'
@@ -694,7 +695,7 @@ def simulate(start, policy, flag):
                 else:
                     cur[0] = 'E'
 
-            elif policy[tuple(cur)] == "STAY":
+            elif curpolicy == "STAY":
                 sim_random = rand.uniform(0,1)
                 if sim_random <= 0.85:
                     cur[0] = 'S'
@@ -702,13 +703,13 @@ def simulate(start, policy, flag):
                 else:
                     cur[0] = 'E'
 
-            elif policy[tuple(cur)] == "GATHER":
+            elif curpolicy == "GATHER":
                     sim_random = rand.uniform(0,1)
                     if sim_random <= 0.75:
                         cur[1] = cur[1] + (cur[1] !=2)
 
         elif cur[0] == 'C':
-            if policy[tuple(cur)] == "DOWN":
+            if curpolicy == "DOWN":
                 sim_random = rand.uniform(0,1)
                 if sim_random <= 0.85:
                     cur[0] = 'S'
@@ -716,7 +717,7 @@ def simulate(start, policy, flag):
                 else:
                     cur[0] = 'E'
 
-            elif policy[tuple(cur)] == "LEFT":
+            elif curpolicy == "LEFT":
                 sim_random = rand.uniform(0,1)
                 if sim_random <= 0.85:
                     cur[0] = 'W'
@@ -724,7 +725,7 @@ def simulate(start, policy, flag):
                 else:
                     cur[0] = 'E'
 
-            elif policy[tuple(cur)] == "UP":
+            elif curpolicy == "UP":
                 sim_random = rand.uniform(0,1)
                 if sim_random <= 0.85:
                     cur[0] = 'N'
@@ -732,10 +733,10 @@ def simulate(start, policy, flag):
                 else:
                     cur[0] = 'E'
 
-            elif policy[tuple(cur)] == "RIGHT":
+            elif curpolicy == "RIGHT":
                 cur[0] = 'E'
 
-            elif policy[tuple(cur)] == "STAY":
+            elif curpolicy == "STAY":
                 sim_random = rand.uniform(0,1)
                 if sim_random <= 0.85:
                     cur[0] = 'C'
@@ -743,16 +744,21 @@ def simulate(start, policy, flag):
                 else:
                     cur[0] = 'E'
 
-            elif policy[tuple(cur)] == "SHOOT":
+            elif curpolicy == "SHOOT":
                 if cur[2] > 0:
                     cur[2] = cur[2] - 1
                     sim_random = rand.uniform(0,1)
                     if sim_random <= 0.5:
                         cur[4] = cur[4] - 25
 
-            elif policy[tuple(cur)] == "HIT":
+            elif curpolicy == "HIT":
                 sim_random = rand.uniform(0,1)
                 if sim_random <= 0.1:
                     cur[4] = cur[4] - 50
 
+print("First:")
+simulate(["W",0,0,"D",100], policy, 0)
+print("\nSecond:")
 simulate(["C",2,0,"R",100], policy, 0)
+print("\nThird:")
+simulate(["E",0,1,"D",50], policy, 0)
